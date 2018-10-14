2f4209ba5aed70eb380efab9c53457fc664f6428
Matias Gutierrez <mgutierrez@Matiass-MacBook-Pro.local>
2018-10-14 13:53:35 -0300

    test minimal correction

--- app/test/people.service.test.js
+++ app/test/people.service.test.js
@@ -3,7 +3,7 @@ const { describe, it } = require('mocha')
 const PeopleService = require('../people/people.service')
 
 describe('People', function () {
-  describe('People Service returnOne() method', function () {
+  describe('Service returnOne() method', function () {
     it('should return 1', function () {
       let peopleService = new PeopleService({ getPeople: () => {} })
 
